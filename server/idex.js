d3eee012362136d104b37d97f6d0153a2c9b5fe6
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2024-04-14 01:33:55 -0300

    send messages

--- server/idex.js
+++ server/idex.js
@@ -1,15 +1,31 @@
 import express from "express"
+import { createServer } from "http"
 import logger from 'morgan'
+import { Server } from "socket.io"
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+
+    socket.on("disconect", () => {
+        console.log("an user has disconnected");
+    })
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg)
+    })
+})
+
 app.use(logger('dev'))
 
 app.get("/", (req, res) => {
-    res.send("<h1>hola</h1>")
+    res.sendFile(process.cwd() + "/client/index.html")
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server runing on port http://localhost:${port}`);
 })
